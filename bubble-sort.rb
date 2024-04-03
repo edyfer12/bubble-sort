f76630f15b10884fe43d62ed80d740fe722cce44
Edin <edyfer2@live.com.au>
2024-04-03 16:24:43 +0200

    Rearrange comment and variable

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -2,10 +2,11 @@
     arr = [6, 5, 3, 1, 8, 7, 4, 2]
     #Declare a sentinel variable, allSwapped and set to false
     allSwapped = false
-    #Declare and set variable counter to 1
-    counter = 1
+    
     #Set a infinite loop until all elements in the array are sorted in ascending order
     until allSwapped == true do
+        #Declare and set variable counter to 1
+        counter = 1
         #Loop from 1 to the last index of the array to compare between two values
         while counter < arr.length do
             #If the left element is greater than the right element, swap the values
