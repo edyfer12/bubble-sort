1b4e6e52e1ef28e7f640c09e8ba970597ae82eb9
Edin <edyfer2@live.com.au>
2024-04-03 16:13:18 +0200

    Add loop

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,5 +1,5 @@
 #Define a method that will sort the array via bubble sort and return a new array
-
+    arr = [6, 5, 3, 1, 8, 7, 4, 2]
     #Declare a sentinel variable, allSwapped and set to false
     allSwapped = false
     #Declare and set variable counter to 1
@@ -7,8 +7,10 @@
     #Set a infinite loop until all elements in the array are sorted in ascending order
     until allSwapped == true do
         #Loop from 1 to the last index of the array to compare between two values
+        while counter < arr.length do
             #If the left element is greater than the right element, swap the values
             #Otherwise, keep iterating
+        end
         #If all values are sorted in ascending order end the infinite loop
     end
     #Return the array
