d1523006319d33a180abcfeef64c9947748cedaa
Edin <edyfer2@live.com.au>
2024-04-03 16:06:55 +0200

    Add comments

---
+++ bubble-sort.rb
@@ -0,0 +1,13 @@
+#Define a method that will sort the array via bubble sort and return a new array
+
+    #Declare a sentinel variable, allSwapped and set to false
+    #Declare and set variable counter to 1
+    #Set a infinite loop until all elements in the array are sorted in ascending order
+        #Loop from 1 to the last index of the array to compare between two values
+            #If the left element is greater than the right element, swap the values
+            #Otherwise, keep iterating
+        #If all values are sorted in ascending order end the infinite loop
+    #Return the array
+
+#Invoke the bubble sort method that returns the array sorted and display to the user
+
